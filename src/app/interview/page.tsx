88e3ef84efcbdfee4e3057366d2a65685f0ce4ac
Vishu Verma <visheshbaund@gmail.com>
2025-11-16 13:44:50 +0530

    Adding error handling in interview dashboard page.

--- src/app/interview/page.tsx
+++ src/app/interview/page.tsx
@@ -1,27 +1,41 @@
-import React from "react";
 import Banner from "./_components/Banner";
 import AllInterviewPage from "./_components/AllInterviewPage";
 import { prisma } from "@/utils/prismaClient";
+import { Loader } from "lucide-react";
+
+export const revalidate = 0; // or set to a number like 60 for ISR
 
 const InterviewDashBoardPage = async () => {
-  const interviews = await prisma.interview.findMany();
+  let interviews = [];
 
-  if (!interviews.length) {
+  try {
+    interviews = await prisma.interview.findMany();
+  } catch (error) {
+    console.error("Prisma Error:", error);
     return (
       <div className="md:px-10 px-3 my-8">
         <Banner />
-        <div className="mt-10 space-y-10">
-          <h1 className="text-6xl font-semibold text-neutral-700">
-            No Interviews
-          </h1>
-        </div>
+        <h1 className="text-4xl mt-16 text-center  text-red-600 font-semibold">
+          <Loader className="animate-spin w-8 h-8 inline" />
+          <span className="ml-2">Failed to load interviews.</span>
+        </h1>
       </div>
     );
   }
+
   return (
     <div className="md:px-10 px-3 my-8">
       <Banner />
-      <AllInterviewPage interviews={interviews} />
+
+      {interviews.length === 0 ? (
+        <div className="mt-10">
+          <h1 className="text-5xl font-semibold text-neutral-700">
+            No Interviews
+          </h1>
+        </div>
+      ) : (
+        <AllInterviewPage interviews={interviews} />
+      )}
     </div>
   );
 };
